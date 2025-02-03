f9af3e077e6df33d6753996e81aac3e55ffe0771
barsonyj <barsony@kkando.hu>
2025-02-03 14:08:40 +0100

    Dotenv change

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let con = null;
 try {
     con = await mysql.createConnection({
         host: process.env.DB_HOST,
-        port: 3306,
-        database: "uditok",
+        port: process.env.DB_PORT,
+        database: process.env.DB_DATABASE,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD
      });
